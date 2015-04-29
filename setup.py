07dc4de7949b48eee5413e1119299a3e621641ba
Ilja Bauer <iljabauer@gmail.com>
2015-04-29 19:28:57 +0200

    Updated setup.py meta informations

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='behave-teamcity',
     version="0.1.23",
     packages=['behave_teamcity', ],
-    url='',
-    license='',
-    author='',
-    author_email='',
-    description='',
+    url='https://github.com/iljabauer/behave-teamcity',
+    license='MIT',
+    author='Ilja Bauer',
+    author_email='i.bauer@cuescience.de',
+    description='TeamCity test report formatter for behave',
     install_requires=["behave>=1.2.5,<=1.3", "teamcity-messages"]
 )
