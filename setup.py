2c7c5dc34df6571afb1e47834dc8b319882dd900
Ilja Bauer <iljabauer@gmail.com>
2015-05-04 07:47:05 +0200

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     author='Ilja Bauer',
     author_email='i.bauer@cuescience.de',
     description='TeamCity test report formatter for behave',
-    install_requires=["behave>=1.2.5,<=1.3", "teamcity-messages"]
+    install_requires=["behave>=1.2.5,<=1.3", "teamcity-messages"],
+    keywords=['testing', 'behave', 'teamcity', 'formatter', 'report'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities"
+    ],
 )
